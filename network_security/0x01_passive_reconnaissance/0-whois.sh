64049db375431edf0a9827d679ea17ed6e76d4d1
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-12-24 16:31:53 +0400

    This is a commit

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/{gsub(/^[ \t]+/,"",$2); if($1~/(Street)/)$2=$2" "; if($1~/(Phone Ext|Fax Ext)/ && $2=="")print $1":,"; else print $1","$2}' > "$1.csv"
+whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/{gsub(/^[ \t]+/,"",$2); if($1~/(Phone Ext|Fax Ext)/)print $1", "; else print $1", "$2}' > "$1.csv"

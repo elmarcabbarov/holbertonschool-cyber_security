c75cf7696debbf4d3564ba17ac2676f2575cf4ad
elmarcabbarov <elmarcabbarov2005@gmail.com>
2026-04-14 06:05:15 -0400

    This is a commit

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,3 @@
+
 #!/bin/bash
-tail -n 1000 auth.log | grep "Failed password" | awk '{print $9}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+grep -E "Failed password|Accepted password" auth.log | awk '{print $(NF-5)}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'

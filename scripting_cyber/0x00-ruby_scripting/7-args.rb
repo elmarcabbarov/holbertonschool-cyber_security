7d418501dba86d6c41246b48a557c545ff17ae3c
elmarcabbarov <elmarcabbarov2005@gmail.com>
2026-05-24 11:58:37 -0400

    This is a commit

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
-# Terminaldan gələn arqumentləri dəqiq formatda çap edən funksiya
+# Terminaldan gələn arqumentləri tab ilə formatlayıb çap edən funksiya
 def print_arguments
   if ARGV.empty?
     puts "No arguments provided."
   else
     puts "Arguments:"
     ARGV.each do |arg|
-      puts "    #{arg}"
+      puts "\t#{arg}"
     end
   end
 end

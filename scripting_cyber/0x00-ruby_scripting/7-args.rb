555d1ebd113bcfcf6d30105a57f6166096e35cac
elmarcabbarov <elmarcabbarov2005@gmail.com>
2026-05-24 12:01:15 -0400

    This is a commit

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,12 +1,11 @@
-#!/usr/bin/env ruby
+# 7-args.rb
 
 def print_arguments
   if ARGV.empty?
     puts "No arguments provided."
   else
-    puts "Arguments:"
-    ARGV.each do |arg|
-      puts "   #{arg}"
+    ARGV.each_with_index do |arg, index|
+      puts "#{index + 1}. #{arg}"
     end
   end
 end

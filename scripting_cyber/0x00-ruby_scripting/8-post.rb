2d8926877d069cd24229c9fea4c2a285abdf5246
elmarcabbarov <elmarcabbarov2005@gmail.com>
2026-05-24 12:45:09 -0400

    This is a commit

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -3,23 +3,21 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-# Göndərilən URL-ə daxil edilən parametrlərlə HTTP POST sorğusu atan funksiya
 def post_request(url, body_params)
   uri = URI.parse(url)
-  
-  # Sorğunun başlığına daxil edilən datanın JSON olduğunu bildiririk
   headers = { 'Content-Type' => 'application/json' }
-  
-  # Datanı JSON mətninə çeviririk
   json_data = JSON.generate(body_params)
   
-  # POST sorğusunu icra edirik (Net::HTTP.post avtomatik olaraq HTTPS-i dəstəkləyir)
   response = Net::HTTP.post(uri, json_data, headers)
 
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
   
-  # Gələn cavabı oxunaqlı (pretty) JSON formatında çap edirik
   parsed_body = JSON.parse(response.body)
-  puts JSON.pretty_generate(parsed_body)
+  
+  if parsed_body.empty?
+    puts "{}"
+  else
+    puts JSON.pretty_generate(parsed_body)
+  end
 end
